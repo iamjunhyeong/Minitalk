f21b77f440c3d58f4a2309422978226d10708c03
iamjunhyeong <dddodo447@gmail.com>
2024-02-18 22:45:59 +0900

    divide client server

--- src/client.c
+++ src/client.c
@@ -6,11 +6,24 @@
 /*   By: junhyeop <junhyeop@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 17:13:48 by junhyeop          #+#    #+#             */
-/*   Updated: 2024/02/18 21:05:53 by junhyeop         ###   ########.fr       */
+/*   Updated: 2024/02/18 22:13:21 by junhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "../inc/client.h"
+
+void	print_error(int type)
+{
+	if (type == 1)
+		ft_putstr_fd("Argument Error\n", 2);
+	else if (type == 2)
+		ft_putstr_fd("pid Error\n", 2);
+	else if (type == 3)
+		ft_putstr_fd("Sigaction1 Error\n", 2);
+	else if (type == 4)
+		ft_putstr_fd("Sigaction2 Error\n", 2);
+	exit(1);
+}
 
 void	send_bit(pid_t pid, char c)
 {
@@ -42,16 +55,15 @@ void	send_str(pid_t pid, char *str)
 	send_bit(pid, '\0');
 }
 
-int main(int argc, char **argv) 
+int	main(int argc, char **argv)
 {
 	pid_t	pid;
-	pid_t	cpid;
 
 	if (argc != 3 || argv[2][0] == '\0')
 		print_error(1);
 	pid = ft_atoi(argv[1]);
-	if (pid < 100 || pid > 9999)
-		print_error(2);
+	// if (pid < 100 || pid > 9999)
+	// 	print_error(2);
 	send_str(pid, argv[2]);
-    return 0;
+	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,11 +6,24 @@
 /*   By: junhyeop <junhyeop@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 20:04:41 by junhyeop          #+#    #+#             */
-/*   Updated: 2024/02/18 19:52:31 by junhyeop         ###   ########.fr       */
+/*   Updated: 2024/02/18 21:53:53 by junhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "../inc/server.h"
+
+void	print_error(int type)
+{
+	if (type == 1)
+		ft_putstr_fd("Argument Error\n", 2);
+	else if (type == 2)
+		ft_putstr_fd("pid Error\n", 2);
+	else if (type == 3)
+		ft_putstr_fd("Sigaction1 Error\n", 2);
+	else if (type == 4)
+		ft_putstr_fd("Sigaction2 Error\n", 2);
+	exit(1);
+}
 
 void	print_pid(void)
 {
@@ -21,11 +34,10 @@ void	print_pid(void)
 	ft_printf("wating a massage....\n");
 }
 
-void my_handler(int sig, siginfo_t *info, void	*context)
+void	my_handler(int sig, siginfo_t *info, void	*context)
 {
 	static int	bit;
 	static char	tmp;
-
 
 	(void)info;
 	(void)context;
