89d0b61a4edcf3906ab9b02d92d784b1b9849e3f
iamjunhyeong <dddodo447@gmail.com>
2024-02-18 21:07:35 +0900

    add type 3, 4

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: junhyeop <junhyeop@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 20:17:55 by junhyeop          #+#    #+#             */
-/*   Updated: 2024/02/17 23:02:17 by junhyeop         ###   ########.fr       */
+/*   Updated: 2024/02/18 18:13:21 by junhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,12 @@
 void	print_error(int type)
 {
 	if (type == 1)
-		ft_print_fd("Argument Error\n", 2);
+		ft_putstr_fd("Argument Error\n", 2);
 	else if (type == 2)
-		ft_print_fd("pid Error\n", 2);
+		ft_putstr_fd("pid Error\n", 2);
+	else if (type == 3)
+		ft_putstr_fd("Sigaction1 Error\n", 2);
+	else if (type == 4)
+		ft_putstr_fd("Sigaction2 Error\n", 2);
 	exit(1);
 }
